837975989d8522ef53d0b7b64f2c45ebdbce9436
Ethan Yu <56643584+qwerqazwsx@users.noreply.github.com>
2025-01-27 15:39:23 +0800

    key value posting

--- server/index.js
+++ server/index.js
@@ -14,12 +14,12 @@ app.use('/api/posts', posts)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'))
-  app.get(/.*/, (req, res) => {
+  app.get('/.*/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
   })
 }
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,34 +1,57 @@
 const express = require('express')
 const redis = require('redis')
 
+let client
+
+async function initializeClient() {
+  if (!client) {
+    client = redis.createClient({
+      username: 'default',
+      password: 'HZJQgqtpFHz132BTXFIcKQSuPDr9nYA0',
+      socket: {
+        host: 'redis-16893.crce178.ap-east-1-1.ec2.redns.redis-cloud.com',
+        port: 16893,
+      },
+    })
+    client.on('error', (err) => console.log('Redis Client Error', err))
+    await client.connect()
+  }
+}
+
+initializeClient()
+
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const client = await connectClient()
-  const posts = await client.get('foo')
+  await initializeClient()
+  var posts = {}
+  const keys = await client.keys('*')
+  //   console.log('keys',keys)
+  for (var i = 0; i < keys.length; i++) {
+    const value = await client.get(keys[i])
+    posts[keys[i]] = value
+  }
+  console.log('posts', posts)
   res.send(posts)
-  res.status(200).send()
+
+  res.status(100).send()
 })
 
 router.post('/', async (req, res) => {
-  const client = await connectClient()
-  await client.set('foo', req.body.text)
+  await initializeClient()
+  await client.set(req.body.key, req.body.value)
   res.status(200).send()
 })
 
-async function connectClient() {
-  const client = redis.createClient({
-    username: 'default',
-    password: 'HZJQgqtpFHz132BTXFIcKQSuPDr9nYA0',
-    socket: {
-      host: 'redis-16893.crce178.ap-east-1-1.ec2.redns.redis-cloud.com',
-      port: 16893,
-    },
-  })
-  client.on('error', (err) => console.log('Redis Client Error', err))
-
-  await client.connect()
-  return client
-}
+// Gracefully shut down the Redis connection on termination
+process.on('SIGINT', async () => {
+  console.log('Shutting down server gracefully...')
+  // Close the Redis client connection
+  if (client) {
+    await client.quit()
+    console.log('Redis client disconnected')
+  }
+  process.exit(0)
+})
 
 module.exports = router
