d3f6f3001576942a7d27e4bbaaf68aedb1b4b104
Ethan Yu <56643584+qwerqazwsx@users.noreply.github.com>
2025-07-04 21:54:18 +0800

    add cache to board.js

--- server/googleSheet.js
+++ server/googleSheet.js
@@ -1,25 +1,25 @@
-const { google } = require('googleapis');
-const path = require('path');
-const fs = require('fs').promises;
+const { google } = require('googleapis')
+const path = require('path')
+const fs = require('fs').promises
 
-const CREDENTIALS_PATH = path.join(__dirname, 'credentials.json');
-const TOKEN_PATH = path.join(__dirname, 'token.json');
+const CREDENTIALS_PATH = path.join(__dirname, 'credentials.json')
+const TOKEN_PATH = path.join(__dirname, 'token.json')
 
 async function authorize() {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const credentials = JSON.parse(content);
-  const { client_secret, client_id, redirect_uris } = credentials.web;
-  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-  const token = await fs.readFile(TOKEN_PATH);
-  oAuth2Client.setCredentials(JSON.parse(token));
-  return oAuth2Client;
+	const content = await fs.readFile(CREDENTIALS_PATH)
+	const credentials = JSON.parse(content)
+	const { client_secret, client_id, redirect_uris } = credentials.web
+	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
+	const token = await fs.readFile(TOKEN_PATH)
+	oAuth2Client.setCredentials(JSON.parse(token))
+	return oAuth2Client
 }
 
 async function readSheet(spreadsheetId, range) {
-  const auth = await authorize();
-  const sheets = google.sheets({ version: 'v4', auth });
-  const res = await sheets.spreadsheets.values.get({ spreadsheetId, range });
-  return res.data.values;
+	const auth = await authorize()
+	const sheets = google.sheets({ version: 'v4', auth })
+	const res = await sheets.spreadsheets.values.get({ spreadsheetId, range })
+	return res.data.values
 }
 
-module.exports = { readSheet };
+module.exports = { readSheet }
--- server/routes/api/board.js
+++ server/routes/api/board.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const router = express.Router();
-const { readSheet } = require('../../googleSheet'); // 確保這個路徑正確指向你的 HomeView.js 檔案
+const express = require('express')
+const router = express.Router()
+const { readSheet } = require('../../googleSheet') // 確保這個路徑正確指向你的 HomeView.js 檔案
 
 // 替換為你的試算表ID與範圍
-const SPREADSHEET_ID = '1SrPn-ypwQjyDvto3t6Jn540a-SwqJ4YroyV7JSgM3cM';
-const RANGE = 'board!A1:Z';
+const SPREADSHEET_ID = '1SrPn-ypwQjyDvto3t6Jn540a-SwqJ4YroyV7JSgM3cM'
+const RANGE = 'board!A1:Z'
+
+// cache 用來存儲 Google Sheets 資料
+let cache = null
+// 初始化 Google Sheets 資料
+async function updateCache(params) {
+	cache = await readSheet(SPREADSHEET_ID, RANGE)
+}
+updateCache()
 
 router.get('/', async (req, res) => {
-  try {
-    const data = await readSheet(SPREADSHEET_ID, RANGE);
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+	try {
+		if (!cache) {
+			await updateCache()
+		}
+		res.json(cache)
+	} catch (err) {
+		res.status(500).json({ error: err.message })
+	}
+})
 
-module.exports = router;
+module.exports = router
