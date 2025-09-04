908ffce8598b40c78cca7e29ac78d5a2fef1bba0
Ethan Yu <56643584+qwerqazwsx@users.noreply.github.com>
2025-09-04 13:28:24 +0800

    fix bugs in reviews.js

--- server/index.js
+++ server/index.js
@@ -11,26 +11,26 @@ app.use(bodyParser.json())
 
 const reviews = require('./routes/api/reviews')
 const book = require('./routes/api/book')
-const board = require('./routes/api/board');
-const events = require('./routes/api/events');
-const records = require('./routes/api/records');
+const board = require('./routes/api/board')
+const events = require('./routes/api/events')
+const records = require('./routes/api/records')
 
 app.use('/api/reviews', reviews)
 app.use('/api/book', book)
-app.use('/api/events', events);
-app.use('/api/board', board);
-app.use('/api/records', records);
+app.use('/api/events', events)
+app.use('/api/board', board)
+app.use('/api/records', records)
 
 // Please comment the following lines if you have not built the frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname + '/dist/'))
-    app.get('/.*/', (req, res) => {
-        res.sendFile(__dirname + '/dist/index.html')
-    })
+	app.use(express.static(__dirname + '/dist/'))
+	app.get('/.*/', (req, res) => {
+		res.sendFile(__dirname + '/dist/index.html')
+	})
 }
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+	console.log(`Server running on port ${port}`)
 })
